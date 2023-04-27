b04a0313208d3cecaf814294430028a1b0e7f1d4
Katerina Bolshakova <e.bolshakova2010@gmail.com>
2023-04-27 13:17:41 +0300

    feat: add function initKeabord, add event listener

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ class Keyboard {
   }
 
   initKeyboard() { // отрисовать клаву
-    function HTMLBody() {// функция по созданию html разметки 
+    function HTMLBody() {
       const HTML_TEMPLATE = `
       <main>
       <section class="body">
@@ -22,7 +22,7 @@ class Keyboard {
           <h2 class="title">RSS Virtual Keyboard</h2>
         </div>
         <div class="area">
-          <textarea class="area-text" rows="10" placeholder="..."></textarea> 
+          <textarea class="area-text" rows="8" placeholder="..."></textarea> 
         </div>
         <div class="keyboard">
           <div class="string1"></div>
@@ -32,133 +32,58 @@ class Keyboard {
           <div class="string5"></div>
         </div>
         <div class="info">
-          <p class="info-text">Language switch: <b>Ctrl left + Alt left</b></p>
-          <p class="info-text">Made in <b>Windows.</b></p>
+          <p class="info-text">Language switch keys: <b>left Ctrl  + left Alt</b> or 🌐</p>
+          <p class="info-text">*implemented on <b>Windows OS</b></p>
           </div>
         </div>
       </section>
       </main>
-      `;
+      <script type="module" src="index.js"></script>`;
       document.body.innerHTML = HTML_TEMPLATE;
     }
+    HTMLBody();
 
-    HTMLBody();//создать html разметку
-    this.textArea = document.querySelector('.text-area');//взять текстареа
+    this.keyString1 = document.querySelector('.string1');
+    this.keyString2 = document.querySelector('.string2');
+    this.keyString3 = document.querySelector('.string3');
+    this.keyString4 = document.querySelector('.string4');
+    this.keyString5 = document.querySelector('.string5');
 
-    this.keyString1 = document.querySelector('.string1');//взять 1 строку
-      for (let i = 0; i <= 13; i ++) {
-        const newBtn = document.createElement('div');
-        const classes = ['key', this.buttons[i].type];
-        newBtn.classList.add(...classes);
-        newBtn.id = this.buttons[i].code;
-        newBtn.dataset.code = this.buttons[i].code;
-  
-        if (!this.buttons[i].keycodeFix) {
-          if (this.adaptation.Shift !== this.adaptation.CapsLock) {
-            newBtn.innerHTML = this.buttons[i].key[`${this.currentLang}Shift`];
-          } else {
-            newBtn.innerHTML = this.buttons[i].key[this.currentLang];
-          }
-        } else {
-          newBtn.innerHTML = this.buttons[i].key.fix;
-        }
-  
-        this.keyString1.appendChild(newBtn);
+    for (let i = 0; i <= 63; i++) {
+      const newBtn = document.createElement('div');
+      const classes = ['key', this.buttons[i].type];
+      newBtn.classList.add(...classes);
+      newBtn.id = this.buttons[i].code;
+      newBtn.dataset.code = this.buttons[i].code;
+
+      if (!this.buttons[i].keycodeFix) {
+        newBtn.innerHTML = this.buttons[i].key[this.currentLang];
+      } else {
+        newBtn.innerHTML = this.buttons[i].key.fix;
       }
 
-    this.keyString2 = document.querySelector('.string2'); //взять 2 строку
-      for (let i = 14; i <= 28; i ++) {
-        const newBtn = document.createElement('div');
-        const classes = ['key', this.buttons[i].type];
-        newBtn.classList.add(...classes);
-        newBtn.id = this.buttons[i].code;
-        newBtn.dataset.code = this.buttons[i].code;
-  
-        if (!this.buttons[i].keycodeFix) {
-          if (this.adaptation.Shift !== this.adaptation.CapsLock) {
-            newBtn.innerHTML = this.buttons[i].key[`${this.currentLang}Shift`];
-          } else {
-            newBtn.innerHTML = this.buttons[i].key[this.currentLang];
-          }
-        } else {
-          newBtn.innerHTML = this.buttons[i].key.fix;
-        }
-  
-        this.keyString2.appendChild(newBtn);
-      }
+      if (i >= 0 && i <= 13) {this.keyString1.appendChild(newBtn);}
+      if (i >= 14 && i <= 28) {this.keyString2.appendChild(newBtn);}
+      if (i >= 29 && i <= 41) {this.keyString3.appendChild(newBtn);}
+      if (i >= 42 && i <= 54) {this.keyString4.appendChild(newBtn);}
+      if (i >= 55 && i <= 63) {this.keyString5.appendChild(newBtn);}
+    }
 
-    this.keyString3 = document.querySelector('.string3');//взять 3 строку
-      for (let i = 29; i <= 41; i ++) {
-        const newBtn = document.createElement('div');
-        const classes = ['key', this.buttons[i].type];
-        newBtn.classList.add(...classes);
-        newBtn.id = this.buttons[i].code;
-        newBtn.dataset.code = this.buttons[i].code;
-  
-        if (!this.buttons[i].keycodeFix) {
-          if (this.adaptation.Shift !== this.adaptation.CapsLock) {
-            newBtn.innerHTML = this.buttons[i].key[`${this.currentLang}Shift`];
-          } else {
-            newBtn.innerHTML = this.buttons[i].key[this.currentLang];
-          }
-        } else {
-          newBtn.innerHTML = this.buttons[i].key.fix;
-        }
-  
-        this.keyString3.appendChild(newBtn);
-      }
+    this.textArea = document.querySelector('.area-text');
+    this.keyboard = document.querySelector('.keyboard');
+    this.btnArr = document.querySelectorAll(".key");
 
-
-    this.keyString4 = document.querySelector('.string4');//взять 4 строку
-      for (let i = 42; i <= 54; i ++) {
-        const newBtn = document.createElement('div');
-        const classes = ['key', this.buttons[i].type];
-        newBtn.classList.add(...classes);
-        newBtn.id = this.buttons[i].code;
-        newBtn.dataset.code = this.buttons[i].code;
-  
-        if (!this.buttons[i].keycodeFix) {
-          if (this.adaptation.Shift !== this.adaptation.CapsLock) {
-            newBtn.innerHTML = this.buttons[i].key[`${this.currentLang}Shift`];
-          } else {
-            newBtn.innerHTML = this.buttons[i].key[this.currentLang];
-          }
-        } else {
-          newBtn.innerHTML = this.buttons[i].key.fix;
-        }
-  
-        this.keyString4.appendChild(newBtn);
-      }
-
-    this.keyString5 = document.querySelector('.string5');//взять 5 строку
-      for (let i = 55; i <= 63; i ++) {
-        const newBtn = document.createElement('div');
-        const classes = ['key', this.buttons[i].type];
-        newBtn.classList.add(...classes);
-        newBtn.id = this.buttons[i].code;
-        newBtn.dataset.code = this.buttons[i].code;
-  
-        if (!this.buttons[i].keycodeFix) {
-          if (this.adaptation.Shift !== this.adaptation.CapsLock) {
-            newBtn.innerHTML = this.buttons[i].key[`${this.currentLang}Shift`];
-          } else {
-            newBtn.innerHTML = this.buttons[i].key[this.currentLang];
-          }
-        } else {
-          newBtn.innerHTML = this.buttons[i].key.fix;
-        }
-  
-        this.keyString5.appendChild(newBtn);
-      }
-      /*window.addEventListener('keydown', f);*/
-      /*window.addEventListener('keyup',f);*/
+    this.keyboard.addEventListener('mousedown', this.pressClick.bind(this));
+    this.keyboard.addEventListener('mouseup', this.pressClickUp.bind(this));
+    window.addEventListener("keydown", this.pressKeyDown.bind(this));
+    window.addEventListener("keyup", this.pressKeyUp.bind(this));
   }
-  
+ 
 }
 
-const objKeyboard = new Keyboard(buttonsArr);
+const virtualKeyboard = new Keyboard(buttonsArr);
 
-objKeyboard.initKeyboard();
+virtualKeyboard.initKeyboard();
 
 
 
